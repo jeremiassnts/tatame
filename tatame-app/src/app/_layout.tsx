9180086772f37d0460678de9125079c56b396ee4
Jeremias Santos <jeremiassnts3@gmail.com>
2025-10-29 15:20:52 -0300

    feat: new login screen

--- tatame-app/src/app/_layout.tsx
+++ tatame-app/src/app/_layout.tsx
@@ -4,17 +4,23 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "react-native-reanimated";
 import { StyleSheet } from "react-native";
 import { COLORS } from "../constants/colors";
+import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <Stack screenOptions={{
-        contentStyle: styles.container,
-        headerShown: false,
-      }}>
-        <Stack.Screen name="login" />
-      </Stack>
-    </ClerkProvider>
+    <GluestackUIProvider mode="dark">
+      <ClerkProvider tokenCache={tokenCache}>
+        <Stack
+          screenOptions={{
+            contentStyle: styles.container,
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="login" />
+        </Stack>
+      </ClerkProvider>
+    </GluestackUIProvider>
   );
 }
 
