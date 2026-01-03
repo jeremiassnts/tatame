c3af50eff8aee658b8cbf5e770a73c43fc5ac0ea
Jeremias Santos <jeremiassnts3@gmail.com>
2026-01-03 18:36:17 -0300

    feat: back with delete user

--- src/components/account-section/index.tsx
+++ src/components/account-section/index.tsx
@@ -2,13 +2,10 @@ import { Card } from "../ui/card";
 import { Heading } from "../ui/heading";
 import { HStack } from "../ui/hstack";
 import { VStack } from "../ui/vstack";
+import { DeleteDialog } from "./delete-dialog";
 import { SignOutDialog } from "./sign-out-dialog";
 
 export function AccountSection() {
-    async function handleDeleteAccount() {
-        console.log("delete account");
-    }
-
     return (
         <Card className="w-full border-2 border-neutral-800 mt-4 bg-neutral-900">
             <HStack className="justify-between items-center mb-4">
@@ -16,7 +13,7 @@ export function AccountSection() {
             </HStack>
             <VStack className="gap-2">
                 <SignOutDialog />
-                {/* <DeleteDialog /> */}
+                <DeleteDialog />
             </VStack>
         </Card>
     )
