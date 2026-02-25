53ecd500d1ddd13b3e0b9b1ff6e57add5d90eaa7
Jeremias Santos <jeremiassnts3@gmail.com>
2026-02-25 11:19:09 -0300

    fix: next class with gym id

--- src/api/classes/get-next-class.tsx
+++ src/api/classes/get-next-class.tsx
@@ -2,18 +2,25 @@ import { useApi } from "@/src/hooks/use-api";
 import { useProfileContext } from "@/src/hooks/use-profile-context";
 import { useToast } from "@/src/hooks/use-toast";
 import { useQuery } from "@tanstack/react-query";
+import { ClassDetails } from "./get-class-by-id";
+
+interface GetNextClassResponse {
+  data: ClassDetails;
+}
 
 export function useGetNextClass() {
   const { get } = useApi();
   const { showErrorToast } = useToast();
-  const { user } = useProfileContext();
+  const { gym } = useProfileContext();
 
   return useQuery({
-    queryKey: ["next-class", user?.id],
+    queryKey: ["next-class", gym?.id],
     queryFn: async () => {
       try {
-        if (!user?.id) return null;
-        const { data } = await get<any>(`/class/next/${user?.id}`);
+        if (!gym?.id) return null;
+        const { data } = await get<GetNextClassResponse>(
+          `/class/next/${gym?.id}`,
+        );
         if (!data) return null;
         return data;
       } catch (error) {
